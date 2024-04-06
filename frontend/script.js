f6494e04bb8207403aefe139c53e229ce837d4e7
Luis Siqueri <luissiquerigg@gmail.com>
2024-04-06 18:31:50 -0300

    estilizando frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,13 @@
 const input = () => document.querySelector("#input-file");
-input.style.opacity = 0;
+const label = document.querySelector('.label-input-file');
+const abovePanel = document.querySelector('.above-panel');
+
+input().style.opacity = 0;
+input().addEventListener("change", function () {
+    if (input().files.length > 0)
+        var fileName = input().files[0].name;
+    label.innerHTML = fileName;
+});
 
 function clickButton() {
     const formData = new FormData();
@@ -8,19 +16,19 @@ function clickButton() {
         method: 'POST',
         body: formData
     }).then(response => response.json())
-    .then(data => {
-        console.log(data)
-        const imgFluxGraph = document.createElement("IMG");
-        imgFluxGraph.src = "http://localhost:8000/graficos/fluxGraph.svg";
-        imgFluxGraph.style.height = "100px";
-        document.querySelector(".body").appendChild(imgFluxGraph);
-        
-        const imgLocationsGraph = document.createElement("IMG");
-        imgLocationsGraph.src = "http://localhost:8000/graficos/locationsGraph.svg";
-        imgLocationsGraph.style.height = "100px";
-        document.querySelector(".body").appendChild(imgLocationsGraph);
-        
-    }).catch(error => {
-        console.log(error);
-    });
+        .then(data => {
+            console.log(data)
+            const imgFluxGraph = document.createElement("IMG");
+            imgFluxGraph.src = "http://localhost:8000/graficos/fluxGraph.svg";
+            imgFluxGraph.style.height = "100px";
+            document.querySelector(".body").appendChild(imgFluxGraph);
+
+            const imgLocationsGraph = document.createElement("IMG");
+            imgLocationsGraph.src = "http://localhost:8000/graficos/locationsGraph.svg";
+            imgLocationsGraph.style.height = "100px";
+            document.querySelector(".body").appendChild(imgLocationsGraph);
+
+        }).catch(error => {
+            console.log(error);
+        });
 }
